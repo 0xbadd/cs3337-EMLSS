386b56a76e5fa1cad246f1deeaa93019c1181bec
Daniel Gomez <47473031+0xbadd@users.noreply.github.com>
2019-02-24 23:32:33 -0800

    Initial Patient class

---
+++ src/backend/Patient.java
@@ -0,0 +1,43 @@
+package backend;
+
+import javafx.geometry.Point2D;
+
+enum Severity {
+    LIFE_THREATING, NON_LIFE_THREATING
+}
+
+public class Patient {
+    private int id;
+    private Point2D location;
+    private Severity injurySeverity;
+
+    public Patient(int id, Point2D location, Severity injurySeverity) {
+        this.id = id;
+        this.location = location;
+        this.injurySeverity = injurySeverity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Point2D getLocation() {
+        return location;
+    }
+
+    public Severity getInjurySeverity() {
+        return injurySeverity;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setLocation(Point2D location) {
+        this.location = location;
+    }
+
+    public void setInjurySeverity(Severity injurySeverity) {
+        this.injurySeverity = injurySeverity;
+    }
+}
