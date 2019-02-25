a8d5d01d0a19e05f661e55c1d6aa5e75d4c691dd
Daniel Gomez <47473031+0xbadd@users.noreply.github.com>
2019-02-24 23:48:29 -0800

    Initial Hospital class

---
+++ src/backend/Hospital.java
@@ -0,0 +1,29 @@
+package backend;
+
+import javafx.geometry.Point2D;
+
+public class Hospital {
+    private int id;
+    private Point2D location;
+
+    public Hospital(int id, Point2D location) {
+        this.id = id;
+        this.location = location;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Point2D getLocation() {
+        return location;
+    }
+
+    public void setLocation(Point2D location) {
+        this.location = location;
+    }
+}
