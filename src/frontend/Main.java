94cb36c38d60c4dbf33bc3a13bdb349e63252a4f
Daniel Gomez <47473031+0xbadd@users.noreply.github.com>
2019-02-24 20:39:51 -0800

    Initial project commit

---
+++ src/frontend/Main.java
@@ -0,0 +1,23 @@
+package frontend;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("gui.fxml"));
+        primaryStage.setTitle("Emergency Medical Logistical Support System");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
