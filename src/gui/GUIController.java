fa70b1223cf90bce8ea028e1bc7ff5bd5cc98a9b
Daniel Gomez <47473031+0xbadd@users.noreply.github.com>
2019-04-13 00:55:05 -0700

    Removed unused imports from GUIController

--- src/gui/GUIController.java
+++ src/gui/GUIController.java
@@ -6,8 +6,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
