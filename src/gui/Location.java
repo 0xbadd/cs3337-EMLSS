a5f442b922fb15cf14c0e5c42ce5b9a4fc3b9fe3
Daniel Gomez <47473031+0xbadd@users.noreply.github.com>
2019-04-12 23:59:55 -0700

    Cleaned up Location

--- src/gui/Location.java
+++ src/gui/Location.java
@@ -3,48 +3,58 @@ package gui;
 import backend.mainController.Point;
 
 public class Location {
-	private int row,column;
-	private char value;
-	private Object object;
-	//integers record position,char value records  what is in the position
-	public Location(Point p,char val, Object inObj) {
-		//constructor for location
-		column=p.getY();
-		row=p.getX();
-		value=val;
-		if(inObj!=null) {
-			object = inObj;
-		}
-	}
-	public int getRow() {
-		return row;
-	}
-	public int getColumn() {
-		return column;
-	}
-	public char getValue() {
-		return value;
-	}
-	public Object getObject() {
-		return object;
-	}
-	public void setRow(int input) {
-		row=input;
-	}
-	public void setColumn(int input) {
-		column = input;
-	}
-	public void setValue(char input) {
-		value=input;
-	}
-	public void setNewLocation(int y,int x,char val) {
-		column=y;
-		row=x;
-		value=val;
-	}
-	public void setObject(Object obj) {
-		object = obj;
-	}
+    private int row, column;
+    private char value;
+    private Object object;
+
+    //integers record position,char value records  what is in the position
+    public Location(Point p, char val, Object inObj) {
+        //constructor for location
+        column = p.getY();
+        row = p.getX();
+        value = val;
+        if (inObj != null) {
+            object = inObj;
+        }
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public char getValue() {
+        return value;
+    }
+
+    public Object getObject() {
+        return object;
+    }
+
+    public void setRow(int input) {
+        row = input;
+    }
+
+    public void setColumn(int input) {
+        column = input;
+    }
+
+    public void setValue(char input) {
+        value = input;
+    }
+
+    public void setNewLocation(int y, int x, char val) {
+        column = y;
+        row = x;
+        value = val;
+    }
+
+    public void setObject(Object obj) {
+        object = obj;
+    }
 
 
 }
