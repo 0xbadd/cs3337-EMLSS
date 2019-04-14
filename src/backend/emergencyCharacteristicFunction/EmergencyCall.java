9694cd34e741e34fe886b434236978f7f7c38a38
Daniel Gomez <47473031+0xbadd@users.noreply.github.com>
2019-04-14 08:58:02 -0700

    Implemented initial emergencyCallGenerator thread

--- src/backend/emergencyCharacteristicFunction/EmergencyCall.java
+++ src/backend/emergencyCharacteristicFunction/EmergencyCall.java
@@ -5,30 +5,21 @@ import backend.mainController.MainController;
 import backend.mainController.Patient;
 import backend.mainController.Point;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 public class EmergencyCall {
     private int time;
     private int numPatients;
     private Point location;
-    private Map<Integer, Patient> patients;
+    private List<Integer> patientIdList;
 
-    public EmergencyCall(int time, int numPatients, Point location) {
+    public EmergencyCall(int time, int numPatients, Point location, List<Integer> patientIdList) {
         this.time = time;
         this.numPatients = numPatients;
         this.location = location;
-
-        for (int i = 0; i < numPatients; i++) {
-            int injurySeverityRoll = (int) (Math.random() * 101);
-            InjurySeverity injurySeverity;
-            if (injurySeverityRoll <= 90) {
-                injurySeverity = InjurySeverity.NON_LIFE_THREATENING;
-            } else {
-                injurySeverity = InjurySeverity.LIFE_THREATENING;
-            }
-
-            patients.put(MainController.createId(), new Patient(location, injurySeverity));
-        }
+        this.patientIdList = patientIdList;
     }
 
     public int getTime() {
@@ -43,7 +34,8 @@ public class EmergencyCall {
         return location;
     }
 
-    public Map<Integer, Patient> getPatients() {
-        return patients;
+    public List<Integer> getPatients() {
+        return patientIdList;
     }
+
 }
