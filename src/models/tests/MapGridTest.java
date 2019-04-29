aeb878b47ccec8e168d3541938173004a7a39124
Daniel Gomez <47473031+0xbadd@users.noreply.github.com>
2019-04-28 18:18:12 -0700

    Fixed getPath method in MapGrid

--- src/models/tests/MapGridTest.java
+++ src/models/tests/MapGridTest.java
@@ -4,6 +4,8 @@ import models.MapGrid;
 import models.Point;
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Stack;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,15 +15,15 @@ class MapGridTest {
     @Test
     void getPath() {
         MapGrid mapGrid = new MapGrid();
-        Point startPoint = new Point(0,0);
-        Point endPoint = new Point(0, 3);
+        Point start = new Point(0,0);
+        Point end = new Point(0, 3);
 
         Stack<Point> expected = new Stack<>();
         expected.push(new Point(0, 3));
         expected.push(new Point(0, 2));
         expected.push(new Point(0, 1));
 
-        Stack<Point> actual = mapGrid.getPath(startPoint, endPoint);
+        Stack<Point> actual = mapGrid.getPath(start, end);
 
         assertEquals(expected, actual);
     }
@@ -30,13 +32,29 @@ class MapGridTest {
     void isValidDrivingLocation() {
         MapGrid mapGrid = new MapGrid();
 
-        assertTrue(mapGrid.isValidDrivingLocation(0, 0));
+        assertTrue(mapGrid.isValidDrivingLocation(new Point(0, 3)));
     }
 
     @Test
     void isValidDrivingLocationFalseCase() {
         MapGrid mapGrid = new MapGrid();
 
-        assertFalse(mapGrid.isValidDrivingLocation(2, 2));
+        assertFalse(mapGrid.isValidDrivingLocation(new Point(2, 2)));
+    }
+
+    @Test
+    void getNeighbors() {
+        MapGrid mapGrid = new MapGrid();
+        Point test = new Point( 3,3);
+
+        List<Point> expected = new LinkedList<>();
+        expected.add(new Point(3, 4));
+        expected.add(new Point(3, 2));
+        expected.add(new Point(2, 3));
+        expected.add(new Point(4, 3));
+
+        List<Point> actual = mapGrid.getNeighbors(test);
+
+        assertEquals(expected, actual);
     }
 }
