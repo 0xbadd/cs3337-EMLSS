a7a6f9eb9553380478f5c38790f427c331d5dad6
Daniel Gomez <47473031+0xbadd@users.noreply.github.com>
2019-04-30 02:53:21 -0700

    Refactored EmergencyCallGenerator

--- src/emergencyCharacteristicFunction/EmergencyCallGenerator.java
+++ src/emergencyCharacteristicFunction/EmergencyCallGenerator.java
@@ -22,42 +22,27 @@ public class EmergencyCallGenerator implements Runnable {
     @Override
     public void run() {
         while (!Thread.currentThread().isInterrupted()) {
-            int spawnTime = (int)(Math.random() * 10 + 1); // 1 - 10 seconds
             try {
+                int spawnTime = (int)(Math.random() * 10 + 1); // 1 - 10 seconds
                 Thread.sleep(spawnTime * 1000);
+
+                int numPatients = getRandomNumPatients();
+                Point emergencyLocation = getRandomEmergencyLocation();
+
+                Map<Integer, Patient> patients = new LinkedHashMap<>();
+                for (int i = 0; i < numPatients; i++) {
+                    patients.put(MainController.createId(), spawnPatient(emergencyLocation));
+                }
+
+                List<Integer> patientIdList = new LinkedList<>(patients.keySet());
+                EmergencyCall emergencyCall = new EmergencyCall(0, numPatients, emergencyLocation, patientIdList);
+
+                emergencyCallDirectory.put(MainController.createId(), emergencyCall);
+                patientDirectory.putAll(patients);
+                patientQueue.addAll(patients.entrySet());
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                Thread.currentThread().interrupt();
             }
-
-            int numPatients;
-            int numPatientsRoll = (int)(Math.random() * 101);
-            if (numPatientsRoll <= 50) {
-                numPatients = 1;
-            } else if (numPatientsRoll <= 70) {
-                numPatients = 2;
-            } else if (numPatientsRoll <= 85) {
-                numPatients = 3;
-            } else if (numPatientsRoll <= 95) {
-                numPatients = 4;
-            } else {
-                numPatients = 5;
-            }
-
-            int emergencyX = (int)(Math.random() * (MapGrid.MAP_SIZE_X + 1));
-            int emergencyY = (int)(Math.random() * (MapGrid.MAP_SIZE_Y + 1));
-            Point emergencyLocation = new Point(emergencyX, emergencyY);
-
-            Map<Integer, Patient> patients = new LinkedHashMap<>();
-            for (int i = 0; i < numPatients; i++) {
-                patients.put(MainController.createId(), spawnPatient(emergencyLocation));
-            }
-
-            List<Integer> patientIdList = new LinkedList<>(patients.keySet());
-            EmergencyCall emergencyCall = new EmergencyCall(0, numPatients, emergencyLocation, patientIdList);
-
-            emergencyCallDirectory.put(MainController.createId(), emergencyCall);
-            patientDirectory.putAll(patients);
-            patientQueue.addAll(patients.entrySet());
         }
     }
 
@@ -71,5 +56,26 @@ public class EmergencyCallGenerator implements Runnable {
         }
 
         return new Patient(location, injurySeverity);
+    }
+
+    private int getRandomNumPatients() {
+        int numPatientsRoll = (int)(Math.random() * 101);
+        if (numPatientsRoll <= 50) {
+            return 1;
+        } else if (numPatientsRoll <= 70) {
+            return 2;
+        } else if (numPatientsRoll <= 85) {
+            return 3;
+        } else if (numPatientsRoll <= 95) {
+            return 4;
+        } else {
+            return 5;
+        }
+    }
+
+    private Point getRandomEmergencyLocation() {
+        int emergencyX = (int)(Math.random() * (MapGrid.MAP_SIZE_X + 1));
+        int emergencyY = (int)(Math.random() * (MapGrid.MAP_SIZE_Y + 1));
+        return new Point(emergencyX, emergencyY);
     }
 }
