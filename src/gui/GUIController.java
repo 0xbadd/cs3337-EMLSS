7aafa7873424a651e574a9f7d18a05f3019e3f8d
Daniel Gomez <47473031+0xbadd@users.noreply.github.com>
2019-04-29 22:08:25 -0700

    Implemented initialize and addAssignment methods to GUIController class

--- src/gui/GUIController.java
+++ src/gui/GUIController.java
@@ -1,5 +1,24 @@
 package gui;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import mainController.MainController;
+
 public class GUIController {
+    public FXCollections entries;
+    private MainController mainController = new MainController();
+    @FXML private TableView<AssignmentEntry> assignmentsTable;
+
+    @FXML
+    private void initialize() {
+        mainController.startAcceptingEmergencyCalls();
+    }
+
+    private void addAssignment(String callID, String patientID, String assignedAmbulanceID, String status) {
+        ObservableList<AssignmentEntry> data = assignmentsTable.getItems();
+        data.add(new AssignmentEntry(callID, patientID, assignedAmbulanceID, status));
+    }
 }
 
