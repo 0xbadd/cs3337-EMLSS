4c31dc673274d9620ba7f62a8ff5d19e74afb5e7
Daniel Gomez <47473031+0xbadd@users.noreply.github.com>
2019-04-29 22:43:25 -0700

    Refactored addAssignment and assigned a prefWidth to columns in gui

--- src/gui/GUIController.java
+++ src/gui/GUIController.java
@@ -1,15 +1,13 @@
 package gui;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
 import mainController.MainController;
 
 public class GUIController {
-    public FXCollections entries;
     private MainController mainController = new MainController();
-    @FXML private TableView<AssignmentEntry> assignmentsTable;
+    @FXML
+    private TableView<AssignmentEntry> assignmentsTable;
 
     @FXML
     private void initialize() {
@@ -17,8 +15,7 @@ public class GUIController {
     }
 
     private void addAssignment(String callID, String patientID, String assignedAmbulanceID, String status) {
-        ObservableList<AssignmentEntry> data = assignmentsTable.getItems();
-        data.add(new AssignmentEntry(callID, patientID, assignedAmbulanceID, status));
+        assignmentsTable.getItems().add(new AssignmentEntry(callID, patientID, assignedAmbulanceID, status));
     }
 }
 
