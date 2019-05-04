94653fcc5829566abec022f2eb54e92b1d33ab4b
Daniel Gomez <47473031+0xbadd@users.noreply.github.com>
2019-05-04 15:05:52 -0700

    Added priority to Patient class

--- src/models/Patient.java
+++ src/models/Patient.java
@@ -3,15 +3,21 @@ package models;
 
 public class Patient {
     private final Point location;
+    private final int priority;
     private final InjurySeverity injurySeverity;
 
-    public Patient(Point location, InjurySeverity injurySeverity) {
+    public Patient(Point location, int priority, InjurySeverity injurySeverity) {
         this.location = location;
+        this.priority = priority;
         this.injurySeverity = injurySeverity;
     }
 
     public Point getLocation() {
         return location;
+    }
+
+    public int getPriority() {
+        return priority;
     }
 
     public InjurySeverity getInjurySeverity() {
