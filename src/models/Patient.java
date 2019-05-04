6293eeeae44aadd41299e61fdd7e39366fd251bb
Daniel Gomez <47473031+0xbadd@users.noreply.github.com>
2019-05-04 15:24:48 -0700

    Added priority to InjurySeverity enum

--- src/models/Patient.java
+++ src/models/Patient.java
@@ -3,21 +3,15 @@ package models;
 
 public class Patient {
     private final Point location;
-    private final int priority;
     private final InjurySeverity injurySeverity;
 
-    public Patient(Point location, int priority, InjurySeverity injurySeverity) {
+    public Patient(Point location, InjurySeverity injurySeverity) {
         this.location = location;
-        this.priority = priority;
         this.injurySeverity = injurySeverity;
     }
 
     public Point getLocation() {
         return location;
-    }
-
-    public int getPriority() {
-        return priority;
     }
 
     public InjurySeverity getInjurySeverity() {
